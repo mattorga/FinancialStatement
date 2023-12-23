7fd77711f80f14e77b88f7b51ec1e7573c435451
mattorga <mattheworga85@gmail.com>
2023-12-23 17:13:33 +0800

    commit

--- Asset.py
+++ Asset.py
@@ -1,31 +1,46 @@
 class Asset:
-    _asset_count = 0  # Class variable to track asset count
-
-    def __init__(self, type):
-        self.type = type
-        Asset._asset_count += 1  # Increment asset count on object creation
+    _asset_count = 0 
+    _total_value = 0
 
     @property
-    def type(self):
-        return self._type
-
-    @type.setter
-    def type(self, type):
-        if type not in ["Stock", "Real Estate", "Cryptocurrency", "Job"]:
-            raise ValueError
-        self._type = type
-
-    @classmethod # To get a class variable at Line 2
-    def get_asset_count(cls):
-        return cls._asset_count  # Access class variable using classmethod
+    def value(self):
+        return Asset._total_value
+    @property
+    def asset_count(self):
+        return Asset._asset_count  
 
 class Stock(Asset):
-    def __init__(self, name, price, quantity):
-        super().__init__("Stock")
+    def __init__(self, symbol, name, high, low, close, volume, url=0):
+        self.type = "Stock"
+        self.symbol = symbol
         self.name = name
-        self.price = price
-        self.quantity = quantity
-
-#stock1 = Stock("GOOG", 15, 20)
-#stock2 = Stock("AAPL", 150, 50)
-#print("Total asset count:", Asset.get_asset_count())
+        self.high = high
+        self.low = low
+        self.close = close
+        self.volume = volume
+        self.url = url
+        self.amount = 0
+        Asset._asset_count += 1  # Increment asset count on object creation
+    
+    def buy(self, n):
+        self.amount += n
+        value_bought = int(n * float(self.close))
+        Asset._total_value += value_bought
+    def sell(self, n):
+        if self.amount < n:
+            return "Invalid amounts to sell"
+        self.amount -= n
+        value_sold = int(n * float(self.close))
+        Asset._total_value -= value_sold
+            
+    @property
+    def amount(self):
+        return self._amount
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount
+    
+    @property
+    def value(self):
+        value = int(self._amount*float(self.close))
+        return f"${value}"
--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 from Asset import Asset, Stock
-from datetime import date
 import requests
+from bs4 import BeautifulSoup
 
 def main():
-    
-    stockPrice("GOOGL")
+    ticker = input("Ticker: ")
+    _symbol, _name, _high, _low, _close, _volume, _url = parseStock(ticker)
+    stock1 = Stock(_symbol, _name, _high, _low, _close, _volume, _url)
 
-def stockPrice(ticker):
+    print("Amount: ", stock1.amount)
+    stock1.buy(10)
+    print("Amount: ", stock1.amount)
+    print("Value: ", stock1.value)
+    stock1.sell(5)
+    print("Amount: ", stock1.amount)
+    print("Value: ", stock1.value)
+    print(Asset._total_value)
+    print(Asset._asset_count)
     
+def parseStock(ticker):
+
     # Download webpage using requests
-    home_url = 'http://eoddata.com/stocklist/TSX/A.htm'   #The URL Address of the webpage we will scrape, i.e. Stocks starting from A
+    home_url = f"http://eoddata.com/stocklist/NASDAQ/{ticker[0]}.htm"
     response = requests.get(home_url)
-    print(response)
+    # Retrieve the HTML document
+    page_contents = response.text
 
+    ## Parse the HTML code using BeautifulSoup library and extract the desired information
+    doc = BeautifulSoup(page_contents, 'html.parser')
 
-    # Parse the HTML code using BeautifulSoup library and extract the desired information
+    tr_parent_ro = doc.find_all('tr',{'class':'ro'}) 
+    tr_parent_re = doc.find_all('tr',{'class':'re'})
 
-    # Building the scraper components
+    for i in range(len(tr_parent_ro)): # Search ro class
+        td_child_ro = tr_parent_ro[i].find_all('td') # Gets the necessary information
+        symbol = td_child_ro[0].find('a').text.strip()
+        if symbol == ticker:
+            name = td_child_ro[1].text.strip()
+            high = td_child_ro[2].text.strip()
+            low = td_child_ro[3].text.strip()
+            close = td_child_ro[4].text.strip()
+            volume = td_child_ro[5].text.strip().replace(',', '') # Here we remove the comma
+            url = "http://eoddata.com/" + td_child_ro[0].find('a')['href'] # Here we append the base url
+            return symbol, name, high, low, close, volume, url
 
-    # Compile the extracted information into a Python list and dictionaries
-
-    # Converting the python dictionaries into Pandas Dataframes
-
-    return ticker
+    for j in range(len(tr_parent_re)):
+        td_child_re = tr_parent_re[j].find_all('td') # Gets the necessary information
+        symbol = td_child_re[0].find('a').text.strip()
+        if symbol == ticker:
+            name = td_child_re[1].text.strip()
+            high = td_child_re[2].text.strip()
+            low = td_child_re[3].text.strip()
+            close = td_child_re[4].text.strip()
+            volume = td_child_re[5].text.strip().replace(',', '') # Here we remove the comma
+            url = "http://eoddata.com/" + td_child_re[0].find('a')['href'] # Here we append the base url
+            return symbol, name, high, low, close, volume, url
+                
+    return "No ticker found"
 
 if __name__ == "__main__":
     main()
