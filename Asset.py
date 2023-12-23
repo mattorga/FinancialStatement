58e6f33606eaa5dc17bb9c4e841aea62e310bda2
mattorga <mattheworga85@gmail.com>
2023-12-23 11:37:30 +0800

    commit

--- Asset.py
+++ Asset.py
@@ -11,17 +11,21 @@ class Asset:
 
     @type.setter
     def type(self, type):
-        if type not in ["Stocks", "Real Estate", "Cryptocurrency", "Job"]:
+        if type not in ["Stock", "Real Estate", "Cryptocurrency", "Job"]:
             raise ValueError
         self._type = type
 
-    @classmethod
+    @classmethod # To get a class variable at Line 2
     def get_asset_count(cls):
         return cls._asset_count  # Access class variable using classmethod
 
 class Stock(Asset):
     def __init__(self, name, price, quantity):
-        super().__init__("Stocks")
+        super().__init__("Stock")
         self.name = name
         self.price = price
         self.quantity = quantity
+
+#stock1 = Stock("GOOG", 15, 20)
+#stock2 = Stock("AAPL", 150, 50)
+#print("Total asset count:", Asset.get_asset_count())
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from Asset import Asset, Stock
+from datetime import date
+import requests
 
 def main():
-    stock1 = Stock("GOOG", 20)
-    stock2 = Stock("AAPL", 150, 50)
+    
+    stockPrice("GOOGL")
 
-    print("Total asset count:", Asset.get_asset_count())
+def stockPrice(ticker):
+    
+    # Download webpage using requests
+    home_url = 'http://eoddata.com/stocklist/TSX/A.htm'   #The URL Address of the webpage we will scrape, i.e. Stocks starting from A
+    response = requests.get(home_url)
+    print(response)
+
+
+    # Parse the HTML code using BeautifulSoup library and extract the desired information
+
+    # Building the scraper components
+
+    # Compile the extracted information into a Python list and dictionaries
+
+    # Converting the python dictionaries into Pandas Dataframes
+
+    return ticker
 
 if __name__ == "__main__":
     main()
