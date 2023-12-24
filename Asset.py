98c5b4c8f96790416be46c57b2b85332cee7fa04
mattorga <mattheworga85@gmail.com>
2023-12-25 00:32:53 +0800

    commit

--- Asset.py
+++ Asset.py
@@ -22,6 +22,48 @@ class Stock(Asset):
         self.amount = 0
         Asset._asset_count += 1  # Increment asset count on object creation
     
+
+    def buy(self, n):
+        self.amount += n
+        value_bought = int(n * float(self.close))
+        Asset._total_value += value_bought
+    def sell(self, n):
+        if self.amount < n:
+            return "Invalid amounts to sell"
+        self.amount -= n
+        value_sold = int(n * float(self.close))
+        Asset._total_value -= value_sold
+            
+    @property
+    def amount(self):
+        return self._amount
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount
+    
+    @property
+    def value(self):
+        value = int(self._amount*float(self.close))
+        return f"${value}"
+
+class Cryptocurrency(Asset):
+    def __init__(self, symbol, name, open, high, low, close, circulating_supply, market_cap, url="None"):
+        self.type = "Cryptocurrency"
+        self.symbol = symbol
+        self.name = name
+        self.high = high
+        self.low = low
+        self.close = close
+        self.circulating_supply = circulating_supply
+        self.market_cap = market_cap
+        self.url = url
+        self.amount = 0
+        Asset._asset_count += 1  # Increment asset count on object creation
+    
+    def __str__(self):
+        str = "Type: {}\nSymbol: {}\nName: {}\nHigh: ${}\nLow: ${}\nClose: ${}\nCirculating Supply: {}\nMarket Cap: {}\nURL: {}".format(self.type, self.symbol, self.name, self.high, self.low, self.close, int(self.circulating_supply), int(self.market_cap), self.url)
+        return str
+
     def buy(self, n):
         self.amount += n
         value_bought = int(n * float(self.close))
--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-from Asset import Asset, Stock
+from Asset import Asset, Stock, Cryptocurrency
+import json
 import requests
 from bs4 import BeautifulSoup
 
 def main():
-    ticker = input("Ticker: ")
-    _symbol, _name, _high, _low, _close, _volume, _url = parseStock(ticker)
-    stock1 = Stock(_symbol, _name, _high, _low, _close, _volume, _url)
 
-    print("Amount: ", stock1.amount)
-    stock1.buy(10)
-    print("Amount: ", stock1.amount)
-    print("Value: ", stock1.value)
-    stock1.sell(5)
-    print("Amount: ", stock1.amount)
-    print("Value: ", stock1.value)
-    print(Asset._total_value)
-    print(Asset._asset_count)
+    ticker = input("Ticker: ")
+    symbol, name, open, high, low, close, cirucalting_supply, market_cap, coin_url = parseCrypto(ticker)
+    crypto = Cryptocurrency(symbol, name, open, high, low, close, cirucalting_supply, market_cap, coin_url)
+    print(crypto)
+    
     
 def parseStock(ticker):
 
     # Download webpage using requests
-    home_url = f"http://eoddata.com/stocklist/NASDAQ/{ticker[0]}.htm"
-    response = requests.get(home_url)
+    URL = f"http://eoddata.com/stocklist/NASDAQ/{ticker[0]}.htm"
+    response = requests.get(URL)
     # Retrieve the HTML document
     page_contents = response.text
 
@@ -54,8 +48,42 @@ def parseStock(ticker):
             volume = td_child_re[5].text.strip().replace(',', '') # Here we remove the comma
             url = "http://eoddata.com/" + td_child_re[0].find('a')['href'] # Here we append the base url
             return symbol, name, high, low, close, volume, url
+
+    #ticker = input("Ticker: ")
+    #_symbol, _name, _high, _low, _close, _volume, _url = parseStock(ticker)
+    #stock1 = Stock(_symbol, _name, _high, _low, _close, _volume, _url)
+
+    #print("Amount: ", stock1.amount)
+    #stock1.buy(10)
+    #print("Amount: ", stock1.amount)
+    #print("Value: ", stock1.value)
+    #stock1.sell(5)
+    #print("Amount: ", stock1.amount)
+    #print("Value: ", stock1.value)
+    #print(Asset._total_value)
+    #print(Asset._asset_count)
                 
     return "No ticker found"
+
+def parseCrypto(ticker):
+    
+    URL = f"https://production.api.coindesk.com/v2/tb/price/ticker?assets={ticker}"
+    response = requests.get(URL)
+    json_data = response.json()
+
+    coin_data = json_data['data'][ticker]
+    symbol = coin_data['iso'],
+    name = coin_data['name'],
+    open = coin_data['ohlc']['o']
+    high = coin_data['ohlc']['h']
+    low = coin_data['ohlc']['l']
+    close = coin_data['ohlc']['c']
+    cirucalting_supply = coin_data['circulatingSupply']
+    market_cap = coin_data['marketCap']
+    
+    coin_url = f"https://www.coindesk.com/price/{name[0].lower().replace(' ', '-')}/"
+
+    return symbol[0], name[0], open, high, low, close, cirucalting_supply, market_cap, coin_url
 
 if __name__ == "__main__":
     main()
